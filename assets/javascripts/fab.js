2f408737980984b9ac7316c4431dcf01b4fadc07
rewphus <rcdurant@gmail.com>
2015-05-01 16:02:12 -0500

    Update fab.js

--- assets/javascripts/fab.js
+++ assets/javascripts/fab.js
@@ -2,18 +2,21 @@ Discourse.PageTracker.current().on('change', function () {
 
         $('.list-controls').on('click', '#create-topic', function(){
             $('#create-topic').addClass('create-clicked');
-            setTimeout(function() {$('#reply-control').removeClass('hidden');},650);
-            setTimeout(function() {$('.wmd-controls').addClass('new-controls');});
+            setTimeout(function() {$('#reply-control').appEvents.trigger("composer:opened");},650);
         });
         
         $('#main').on('click', '.cancel', function(){
             $('#create-topic').removeClass('create-clicked');
             $('#reply-control').addClass('hidden');
+            setTimeout(function() {$('#reply-control').removeClass('hidden');},650);
+            $('#reply-control').appEvents.trigger("composer:closed");
         });
         
         $('#main').on('click', '.toggler', function(){
             $('#create-topic').removeClass('create-clicked');
             $('#reply-control').addClass('hidden');
+            setTimeout(function() {$('#reply-control').removeClass('hidden');},650);
+            $('#reply-control').appEvents.trigger("composer:closed");
         });
 
     });
